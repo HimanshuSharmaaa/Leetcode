38b5c74f88f862e9c4d47c3582fa0078c5cb13eb
HimanshuSharmaaa <138246953+HimanshuSharmaaa@users.noreply.github.com>
2026-07-12 15:19:03 +0530

    Time: 35 ms (88.64%), Space: 43.8 MB (64.79%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        map<int,int> mpp;
+        unordered_map<int, int> mpp;
         vector<int> sorted = arr;
         sort(sorted.begin(), sorted.end());
         int rank = 1;
 
-        for(int i = 0; i < sorted.size(); i++){
-            if(i > 0 && sorted[i] != sorted[i-1]) rank++;
+        for (int i = 0; i < sorted.size(); i++) {
+            if (i > 0 && sorted[i] != sorted[i - 1]) rank++;
             mpp[sorted[i]] = rank;
         }
 
-        for(int i = 0; i < arr.size(); i++) arr[i] = mpp[arr[i]];
+        for (int i = 0; i < arr.size(); i++) arr[i] = mpp[arr[i]];
         return arr;
     }
 };
+
+// unordered_map<int, int> map;
+//         vector<int> st = arr;
+//         sort(st.begin(), st.end());
+//         int i = 1;
+//         for (int s : st) {
+//             if(map.find(s)==map.end()){
+
+//             map[s] = i;
+//             i++;
+//             }
+//         }
+//         for (int i = 0; i < arr.size(); i++) {
+//             arr[i] = map[arr[i]];
+//         }
+//         return arr;
