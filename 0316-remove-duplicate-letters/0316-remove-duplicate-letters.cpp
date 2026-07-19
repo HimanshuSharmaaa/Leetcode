d0b1151d1fd1b57511d4de26f089d5c51eb81822
HimanshuSharmaaa <138246953+HimanshuSharmaaa@users.noreply.github.com>
2026-07-19 17:06:19 +0530

    Time: 0 ms (100%), Space: 9.3 MB (68.83%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<bool> taken(26, false);
+        vector<int> mpp(26, 0);
+        string result = "";
+
+        for(int i = 0; i < s.size(); i++) mpp[s[i] - 'a'] = i;
+
+        for(int i = 0; i < s.size(); i++){
+
+            if(taken[s[i]-'a']) continue;
+
+            while(result.size() > 0 && result.back() > s[i] && mpp[result.back()-'a'] > i) {
+                taken[result.back()-'a'] = false;
+                result.pop_back();
+            } 
+
+            taken[s[i] - 'a'] = true;
+            result += s[i];
+        }   
+
+        return result;
+    }
+};
