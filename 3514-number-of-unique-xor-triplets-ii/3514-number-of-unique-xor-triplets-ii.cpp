1cb89bfcc8c00cc406853135e3563602f21fa689
HimanshuSharmaaa <138246953+HimanshuSharmaaa@users.noreply.github.com>
2026-07-24 17:58:05 +0530

    Time: 1011 ms (57.3%), Space: 61.3 MB (55.62%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        unordered_set<int> st1, st2;
+        int largest = accumulate(nums.begin(), nums.end(), 0);
+        int T = 1, count = 0;
 
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = i; j < nums.size(); j++){
-                st1.insert(nums[i]^nums[j]);
+        while (T <= largest) T *= 2;
+
+        vector<bool> st1(T, false), st2(T, false);
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i; j < nums.size(); j++) {
+                st1[nums[i] ^ nums[j]] = true;
             }
         }
 
-        for(int i = 0; i < nums.size(); i++){
-            for(auto &val : st1) {
-                st2.insert(val^nums[i]);
+        for (int i = 0; i < st1.size(); i++) {
+            if (st1[i]) {
+                for (int j = 0; j < nums.size(); j++) {
+                    st2[i ^ nums[j]] = true;
+                }
             }
         }
 
-        return st2.size();
+        for(int i = 0; i < st2.size(); i++) if(st2[i]) count++;
+        return count;
     }
 };
