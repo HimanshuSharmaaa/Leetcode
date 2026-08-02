cb7be2e9faf3594890dbc5a36d5f21682a778cbe
HimanshuSharmaaa <138246953+HimanshuSharmaaa@users.noreply.github.com>
2026-08-02 11:29:55 +0530

    Time: 16 ms (35.04%), Space: 292.9 MB (67.52%) - LeetHub

---
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> dec(nums.size(), 0);
+
+        for (int i = 0; i < queries.size(); i++) {
+            dec[queries[i][0]]--;
+            if (queries[i][1] + 1 < nums.size()) dec[queries[i][1] + 1]++;
+        }
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(i > 0) dec[i] += dec[i-1];
+            nums[i] += dec[i];
+        }
+
+        for(int i : nums) if(i > 0) return false;
+        return true;
+    }
+};
