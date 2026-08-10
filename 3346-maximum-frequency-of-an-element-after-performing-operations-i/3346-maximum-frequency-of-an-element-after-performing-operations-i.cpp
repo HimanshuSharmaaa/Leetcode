2e7caa236674b0e4d8d51c01eba47ddc53604008
HimanshuSharmaaa <138246953+HimanshuSharmaaa@users.noreply.github.com>
2026-08-10 09:42:33 +0530

    Time: 715 ms (13.2%), Space: 205.3 MB (19.12%) - LeetHub

--- 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        int max_val = *max_element(nums.begin(), nums.end());
-        int maxSum = 1, cumSum = 0;
+        int max_val = *max_element(nums.begin(), nums.end()) + k;
         unordered_map<int, int> freq;
-        map<int,int> diff;
+        int ans = 1, cumSum = 0;
+        map<int, int> diff;
 
-        for(int i = 0; i < nums.size(); i++) {
-            freq[nums[i]]++;
+        for (int val : nums) {
+            freq[val]++;
 
-            int l = max(nums[i]-k, 0), r = min(nums[i]+k, max_val);
-            diff[l]++, diff[r+1]--;
-            diff[nums[i]] += 0;
+            int l = max(0, val - k), r = min(val + k, max_val);
+            diff[l]++;
+            diff[r + 1]--;
+            diff[val] += 0;
         }
 
         for(auto it = diff.begin(); it != diff.end(); it++) {
             it->second += cumSum;
             cumSum = it->second;
 
-            int remainig = it->second - freq[it->first];
-            maxSum = max(min(remainig, numOperations) + freq[it->first], maxSum);
-        }        
+            int needed = it->second - freq[it->first];
+            ans = max(ans, min(needed, numOperations) + freq[it->first]);
+        }
 
-        return maxSum;
+        return ans;
     }
 };
