3cabc996e3fb024b14d30b3ed891228fb8fe5064
HimanshuSharmaaa <138246953+HimanshuSharmaaa@users.noreply.github.com>
2026-08-10 12:39:49 +0530

    Time: 15 ms (92.33%), Space: 99.2 MB (90.49%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -6,16 +6,21 @@ public:
         for (int i = 0; i < q.size(); i++) {
             int r1 = q[i][0], c1 = q[i][1], r2 = q[i][2], c2 = q[i][3];
 
-            for (int i = r1; i <= r2; i++) result[i][c1]++;
-
-            if (c2 + 1 < n) {
-                for (int i = r1; i <= r2; i++) result[i][c2+1]--;
-            }
+            result[r1][c1]++;
+            if (c2 + 1 < n) result[r1][c2+1]--;
+            if (r2 + 1 < n) result[r2+1][c1]--;
+            if (c2 + 1 < n && r2 + 1 < n) result[r2+1][c2+1]++;
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < n; j++) {
                 result[i][j] += result[i][j - 1];
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 1; j < n; j++) {
+                result[j][i] += result[j-1][i];
             }
         }
 
