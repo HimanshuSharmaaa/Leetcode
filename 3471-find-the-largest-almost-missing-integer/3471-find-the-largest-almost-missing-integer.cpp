aa2a43e1db468fcb08ea97b6dc7c482369de8a28
HimanshuSharmaaa <138246953+HimanshuSharmaaa@users.noreply.github.com>
2026-08-18 13:14:15 +0530

    Time: 4 ms (46.24%), Space: 29.1 MB (73.68%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
+        int ans = -1, n = nums.size();
         vector<int> mpp(51, 0);
-        int ans = -1;
 
-        if (k == 1 || k == nums.size()) {
-            for (int i = 0; i < nums.size(); i++) mpp[nums[i]]++;
+        for (int i = 0; i < n; i++) mpp[nums[i]]++;
 
+        if (k == 1 || k == n) {
             if(k == 1) {
-                for (int i = 0; i < mpp.size(); i++) if (mpp[i] == 1) ans = max(ans, i);
+                for (int i = 0; i < 51; i++) if (mpp[i] == 1) ans = max(ans, i);
                 return ans;
             } else {
-                for (int i = 0; i < mpp.size(); i++) if (mpp[i] > 0) ans = max(ans, i);
+                for (int i = 0; i < 51; i++) if (mpp[i] > 0) ans = max(ans, i);
                 return ans;    
             }
         }
 
-        for (int i = 0; i <= nums.size() - k; i++) {
-            for (int j = i; j < i+k; j++) mpp[nums[j]]++;
-        }
-
-        for (int i = 0; i < mpp.size(); i++) {
-            if (mpp[i] == 1) ans = max(ans, i);
-        }
-
+        if (mpp[nums[0]] == 1) ans = max(ans, nums[0]);
+        if (mpp[nums[n - 1]] == 1) ans = max(ans, nums[n - 1]);  
         return ans;
     }
 };
