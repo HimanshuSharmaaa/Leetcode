d152e7cc7f8e59eaa42e38c7525ba4f30af95e67
HimanshuSharmaaa <138246953+HimanshuSharmaaa@users.noreply.github.com>
2026-08-31 17:35:13 +0530

    Time: 1 ms (75.76%), Space: 124.7 MB (60.57%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        if(!head->next->next) return {-1,-1};
+        if (!head->next->next) return {-1, -1};
 
-        int prev = head->val, curr = head->next->val, next, mi = INT_MAX, ma, len = 0;
         ListNode* temp = head->next->next;
-        vector<int> points;
+        int prev = head->val, curr = head->next->val, next, mi = INT_MAX, ma,
+            len = 0, fc = -1, cc = -1;
 
-        while(temp) {
-            next = temp->val;
-            if(prev < curr && curr > next) points.push_back(len);
-            if(prev > curr && curr < next) points.push_back(len);
+        while (temp) {
+            if (prev < curr && curr > temp->val ||
+                prev > curr && curr < temp->val) {
+                if (fc == -1) fc = len;
+                if(cc != -1) mi = min(mi, len - cc);
+                cc = len;
+            }
 
-            temp = temp->next;
             prev = curr;
-            curr = next;
+            curr = temp->val;
+            temp = temp->next;
             len++;
         }
 
-        if(points.size() < 2) return {-1,-1};
-        for(int i = 1; i < points.size(); i++) {
-            if(i == points.size()-1) ma = points[i] - points[0];
-            mi = min(mi, points[i] - points[i-1]);
-        }
-
-        return {mi, ma};
+        if (mi == INT_MAX)
+            return {-1, -1};
+        return {mi, cc - fc};
     }
 };
