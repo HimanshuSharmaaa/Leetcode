ed9d963db4d9df5f6faa26a0a4a006ff93d53760
HimanshuSharmaaa <138246953+HimanshuSharmaaa@users.noreply.github.com>
2026-09-05 10:50:42 +0530

    Time: 8 ms (70.04%), Space: 202.5 MB (85.02%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        int n = nums.size(), right = INT_MAX, left = 0, stable = -1;
-        vector<int> rightSum(n, INT_MAX);
+        int right = INT_MAX, left = 0;
+        vector<int> rightSum(nums.size());
 
-        for(int i = n-1; i > -1; i--) {
-            right = min(right, nums[i]);
-            rightSum[i] = min(rightSum[i], right);
+        for(int i = nums.size()-1; i > -1; i--) {
+            if(nums[i] < right) right = nums[i];
+            rightSum[i] = right;
         }
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < nums.size(); i++) {
             left = max(left, nums[i]);
-            if(left- min(rightSum[i], nums[i]) <= k) {
-                stable = i;
-                break;
-            }
+            if(left - min(rightSum[i], nums[i]) <= k) return i;
         }
 
-        return stable;
+        return -1;
     }
 };
